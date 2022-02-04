206d65b1cb7509b2c31d3253d6c6eccf7e82459e
jose13001 <josecarlos_01_30@unitec.edu>
2022-02-04 14:41:44 -0600

    transportista

--- src/lab3p2_jose/carlos_bryan_espinal/conductores.java
+++ src/lab3p2_jose/carlos_bryan_espinal/conductores.java
@@ -1,10 +1,24 @@
 
 package lab3p2_jose.carlos_bryan_espinal;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 /**
  *
  * @author bryan
  */
-public class conductores {
+public class conductores extends personas {
+    static Scanner lea = new Scanner(System.in);
+    ArrayList<conductores>listaconductores=new ArrayList<conductores>();
+    public int experiencia;
+
+    public static void registrarConductor(){
+        System.out.println("ingrese años de experiencia");
+        int experiencia = lea.nextInt();
+        System.out.println("Ingrese apodo");
+        String nombre = lea.nextLine();
+        
+        listaconductores.add(new conductores(experiencia,nombre));
+    }
     
 }
