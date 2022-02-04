abe96cc3d105b494bd37cee5746465f0546e6e2e
Bryan23453 <rayan.andino@gmail.com>
2022-02-04 14:11:41 -0600

    arreglo menu

--- src/lab3p2_jose/carlos_bryan_espinal/Lab3P2_JoseCarlos_Bryan_Espinal.java
+++ src/lab3p2_jose/carlos_bryan_espinal/Lab3P2_JoseCarlos_Bryan_Espinal.java
@@ -11,72 +11,78 @@ import java.util.Scanner;
 public class Lab3P2_JoseCarlos_Bryan_Espinal {
 static Scanner lea = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("0. Salir\n" +
-        "1. Crear Clase\n" +
-        "2. Crear Ruta\n" +
-        "3. Crear Alumno\n" +
-        "4. Agregar Clase a Alumno\n" +
-        "5. Crear Transportista\n" +
-        "6. Crear Transporte\n" +
-        "7. Simulación\n" +
-        "8. Listar Clases\n" +
-        "9. Listar Rutas\n" +
-        "10. Listar Alumnos\n" +
-        "11. Listar Transportistas\n" +
-        "12. Listar Transportes");
-        int op=lea.nextInt();
-        switch (op){
-            case 1:{
-                
+        boolean a=true;
+        while (a==true) {
+            System.out.println("0. Salir\n" +
+            "1. Crear Clase\n" +
+            "2. Crear Ruta\n" +
+            "3. Crear Alumno\n" +
+            "4. Agregar Clase a Alumno\n" +
+            "5. Crear Transportista\n" +
+            "6. Crear Transporte\n" +
+            "7. Simulación\n" +
+            "8. Listar Clases\n" +
+            "9. Listar Rutas\n" +
+            "10. Listar Alumnos\n" +
+            "11. Listar Transportistas\n" +
+            "12. Listar Transportes");
+            int op=lea.nextInt();
+            switch (op){
+                case 0:{
+                    a=false;
+                }
+                break;
+                case 1:{
+
+                }
+                break;
+                case 2:{
+
+                }
+                break;
+                case 3:{
+
+                }
+                break;
+                case 4:{
+
+                }
+                break;
+                case 5:{
+
+                }
+                break;
+                case 6:{
+
+                }
+                break;
+                case 7:{
+
+                }
+                break;
+                case 8:{
+
+                }
+                break;
+                case 9:{
+
+                }
+                break;
+                case 10:{
+
+                }
+                break;
+                case 11:{
+
+                }
+                break;
+                case 12:{
+
+                }
+                break;
+
+
             }
-            break;
-            case 2:{
-                
-            }
-            break;
-            case 3:{
-                
-            }
-            break;
-            case 4:{
-                
-            }
-            break;
-            case 5:{
-                
-            }
-            break;
-            case 6:{
-                
-            }
-            break;
-            case 7:{
-                
-            }
-            break;
-            case 8:{
-                
-            }
-            break;
-            case 9:{
-                
-            }
-            break;
-            case 10:{
-                
-            }
-            break;
-            case 11:{
-                
-            }
-            break;
-            case 12:{
-                
-            }
-            break;
-            
-            
-        }
+        } 
     }
-    
 }
